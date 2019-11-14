02163cb41ef05d1bbc2f4d8b8bd4a03108679b12
= <=>
2019-11-14 14:59:50 +0800

    use pan-dl submodule

--- aria2/aux-files/entry.sh
+++
@@ -1,8 +0,0 @@
-groupmod -o -g ${GID} www-data
-usermod -o -u ${UID} www-data
-
-chown -R www-data:www-data /downloads
-
-su -c /usr/bin/aria2c --conf-path="" --enable-rpc --rpc-listen-all www-data
-
-"${@-sh}"
